8de931d3e321038cece508ae58efdc778e4ba9af
segfaulty <segfaulty@proton.me>
2022-07-02 20:11:42 +0100

    dd

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,7 +5,7 @@ const weatherAPP = (() => {
             key: '775f80930103eb948ca48290efbf90f1',
         },
         giphy: {
-            addr: 'dono',
+            addr: 'ono',
             key: '',
         },
     };
