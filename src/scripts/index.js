689d5277c66740b2f78283bc7fed184a290d8a50
segfaulty <segfaulty@proton.me>
2022-07-03 16:38:55 +0100

    fresh start

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,8 @@ const weatherAPP = (() => {
             key: '775f80930103eb948ca48290efbf90f1',
         },
         giphy: {
-            addr: 'address',
-            key: 'key',
+            addr: 'start',
+            key: 'start',
         },
     };
     const fetchWeatherAPI = ({
