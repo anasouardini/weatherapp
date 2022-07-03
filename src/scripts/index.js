7952164451452f78df070e9bfdcb2403f7ef1cb5
segfaulty <segfaulty@proton.me>
2022-07-03 16:14:58 +0100

    key null

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,7 @@ const weatherAPP = (() => {
         },
         giphy: {
             addr: 'null',
-            key: 'key',
+            key: 'null',
         },
     };
     const fetchWeatherAPI = ({
